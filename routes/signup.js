ad0c33dfab4e47c1dd595f1b22c7528635800db7
Jesstin Swadley <jesstinswadley1@gmail.com>
2019-01-02 20:01:45 -0800

    Add salt and hash to signup.js through bcryptjs

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,6 @@
 const express   = require('express');
 const router    = express.Router();
+const bcrypt    = require('bcryptjs');
 
 router.get('/', (req, res) => {
   res.render('signup');
@@ -10,8 +11,14 @@ router.post('/', (req, res) => {
       username: req.body.username,
       password: req.body.password
     });
-  
-    newUser.save().then(user => res.redirect('/'));
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save().then(user => res.redirect('/'));
+      });
+    });
     console.log("A new has been created");
   });
 
