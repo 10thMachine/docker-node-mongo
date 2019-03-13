2eb06762338f5cf066ec2243005a5b936b51fd64
Jesstin Swadley <jesstinswadley1@gmail.com>
2019-03-13 11:58:41 -0700

    Removing things for mongo aws docker

--- models/User.js
+++ models/User.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+// const UserSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
 
-module.exports = User = mongoose.model('user', UserSchema);
+// module.exports = User = mongoose.model('user', UserSchema);
--- routes/signup.js
+++ routes/signup.js
@@ -1,33 +1,33 @@
-const express   = require('express');
-const router    = express.Router();
-const bcrypt    = require('bcryptjs');
+// const express   = require('express');
+// const router    = express.Router();
+// const bcrypt    = require('bcryptjs');
 
-// Check the User Schema
-const User  = require('../models/User');
+// // Check the User Schema
+// const User  = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.render('signup');
-});
+// router.get('/', (req, res) => {
+//   res.render('signup');
+// });
 
-router.post('/', (req, res) => {
-    const newUser = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
+// router.post('/', (req, res) => {
+//     const newUser = new User({
+//       username: req.body.username,
+//       password: req.body.password
+//     });
     
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if(err) console.log(err);
-        newUser.password = hash;
-        console.log(`${newUser.username}`);
-        console.log(`${newUser.password}`);
-        newUser
-          .save()
-          .then(user => { res.redirect('/'); })
-          .catch(err => console.log(err));
-      });
-    });
-    console.log("A new user has been created");
-  });
+//     bcrypt.genSalt(10, (err, salt) => {
+//       bcrypt.hash(newUser.password, salt, (err, hash) => {
+//         if(err) console.log(err);
+//         newUser.password = hash;
+//         console.log(`${newUser.username}`);
+//         console.log(`${newUser.password}`);
+//         newUser
+//           .save()
+//           .then(user => { res.redirect('/'); })
+//           .catch(err => console.log(err));
+//       });
+//     });
+//     console.log("A new user has been created");
+//   });
 
-module.exports = router;
+// module.exports = router;
